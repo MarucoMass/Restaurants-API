8797e542eec943498367834ee98742531e1d9fd1
MarucoMass <mariomass23@gmail.com>
2024-12-20 03:06:33 -0300

    Add FluentValidation for createRestaurantDto

--- Restaurants.Application/Models/DTOs/CreateRestaurantDTO.cs
+++ Restaurants.Application/Models/DTOs/CreateRestaurantDTO.cs
@@ -1,14 +1,25 @@
-﻿namespace Restaurants.Application.Models.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants.Application.Models.DTOs;
 
 public class CreateRestaurantDTO
 {
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public string? Category { get; set; }
+    //[StringLength(100, MinimumLength = 3)]
+    public string? Name { get; set; } = default!;
+    public string? Description { get; set; } = default!;
+
+    //[Required(ErrorMessage = "Insert a valid category")]
+    public string? Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
+
+    //[EmailAddress(ErrorMessage = "Please insert a valid email")]
     public string? ContactEmail { get; set; }
+
+    [Phone(ErrorMessage = "Please insert a valid phone number")]
     public string? ContactNumber { get; set; }
     public string? City { get; set; }
     public string? Street { get; set; }
+
+    //[RegularExpression(@"^\d{2}-\d{3}$", ErrorMessage = "Insert a valid postal code: XX-XXX")]
     public string? PostalCode { get; set; }
 }
--- Restaurants.Application/Models/DTOs/RestaurantsDTO.cs
+++ Restaurants.Application/Models/DTOs/RestaurantsDTO.cs
@@ -12,9 +12,9 @@ namespace Restaurants.Application.Models.DTOs;
 public class RestaurantsDTO
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public string? Category { get; set; }
+    public string? Name { get; set; } = default!;
+    public string? Description { get; set; } = default!;
+    public string? Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
 
     public string? City { get; set; }
