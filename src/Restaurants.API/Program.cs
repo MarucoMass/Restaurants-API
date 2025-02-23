b5b726e3188e1c33d5e8d8e2701e6a09d47142e6
MarucoMass <mariomass23@gmail.com>
2025-02-23 20:52:56 -0300

    added more controllers testing

--- src/Restaurants.API/Program.cs
+++ src/Restaurants.API/Program.cs
@@ -6,57 +6,46 @@ using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
 
-try
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.AddPresentation();
+
+builder.Services.AddApplication();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+
+await seeder.Seed();
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.UseSerilogRequestLogging();
+
+if (app.Environment.IsDevelopment())
 {
-    var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
+    app.UseSwagger();
 
-    builder.AddPresentation();
-
-    builder.Services.AddApplication();
-
-    builder.Services.AddInfrastructure(builder.Configuration);
-
-    var app = builder.Build();
-
-    var scope = app.Services.CreateScope();
-    var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
-
-    await seeder.Seed();
-
-    app.UseMiddleware<ErrorHandlingMiddleware>();
-
-    app.UseSerilogRequestLogging();
-
-    if (app.Environment.IsDevelopment())
-    {
-
-        app.UseSwagger();
-
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.MapGroup("api/identity")
-        .WithTags("Identity")
-        .MapIdentityApi<User>();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
-
+    app.UseSwaggerUI();
 }
-catch(Exception ex)
-{
-    Log.Fatal(ex, "Application startup failed");
-}
-finally
-{
-    Log.CloseAndFlush();
-}
+
+app.UseHttpsRedirection();
+
+app.MapGroup("api/identity")
+    .WithTags("Identity")
+    .MapIdentityApi<User>();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
 
 public partial class Program { }
